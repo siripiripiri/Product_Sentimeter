458b519707549da6e4fe02f9bbffdcf86fc35475
Algeo3603 <alangeorge3603@gmail.com>
2024-02-20 23:46:28 +0530

    stop

---
+++ amazon_scraper.py
@@ -0,0 +1,43 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+
+headers = {
+    'authority': 'www.amazon.com',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'accept-language': 'en-US,en;q=0.9,bn;q=0.8',
+    'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="102", "Google Chrome";v="102"',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'
+}
+
+reviews_url = 'https://www.amazon.in/Colgate-Gentle-Enamel-Ultra-Toothbrush/product-reviews/B08G5GPXHR/'
+len_pages = 10
+
+def reviewsHtml(url, len_page):
+    
+    # Empty List define to store all pages html data
+    soups = []
+    
+    # Loop for gather all 3000 reviews from 300 pages via range
+    for page_no in range(1, len_page + 1):
+        
+        # parameter set as page no to the requests body
+        params = {
+            'ie': 'UTF8',
+            'reviewerType': 'all_reviews',
+            'filterByStar': 'critical',
+            'pageNumber': page_no,
+        }
+        
+        # Request make for each page
+        response = requests.get(url, headers=headers)
+        
+        # Save Html object by using BeautifulSoup4 and lxml parser
+        soup = BeautifulSoup(response.text, 'lxml')
+        
+        # Add single Html page data in master soups list
+        soups.append(soup)
+        
+    return soups
+
+html_pages = reviewsHtml(reviews_url, len_page)
