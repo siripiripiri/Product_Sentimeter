fe6505a7bba235bb4ffc7eca98f041930cb87a15
Algeo3603 <alangeorge3603@gmail.com>
2024-02-21 09:10:49 +0530

    changes

--- app.py
+++ app.py
@@ -162,6 +162,14 @@ if selected_page == "Main Page":
         else:
             st.warning("Please enter a valid URL.")
 
+        st.download_button(
+            "Download generated file",
+            data = "realtime.csv",
+            file_name= "download.csv",
+            mime = "text/csv",
+            help = "Click here to download a CSV file"
+        )
+
         positive = ""
         positive_count = 0
         negative = ""
@@ -199,19 +207,6 @@ if selected_page == "Main Page":
             choice = random.choice(adjective_list)
             if choice not in display_adjectives:
                 display_adjectives.append(choice)
-<<<<<<< HEAD
-=======
-        # print(display_adjectives)
-        import streamlit as st
-
-        st.download_button(
-            label = "Download CSV",
-            data = "realtime.csv",
-            file_name = "download.csv",
-            mime="text/plain",
-            help='Click here to download the data as a CSV file'
-        )
->>>>>>> 2c8fb1c438db844ce5d5cd2fba7fc2061add227a
 
         total1, total2, total3, total4 = st.columns(4)
         with total1:
@@ -244,33 +239,10 @@ if selected_page == "Main Page":
         st.markdown('</div>', unsafe_allow_html=True)
 
 
-<<<<<<< HEAD
         labels=["Postive","Negative","Neutral"] 
         sizes=[positive_count, negative_count, neutral_count]
         # plt.pie(sizes, labels=labels, autopct='%1.1f%%')
         # st.pyplot()
-=======
-
-
-
-        total1, total2, total3, total4 = st.columns(4)
-        with total1:
-            st.info('Total Number of Reviews', icon="⭐")
-            st.metric("Overall Reviews", number_of_reviews)
-        with total2:
-            st.info('Average Positivity Rate', icon="😄")
-            st.metric("Positivity Rate", positivity_rate)
-        with total3:
-            st.info('Average Neutrality Rate', icon="😐")
-            st.metric("Neutrality Rate", neutrality_rate)
-        with total4:
-            st.info('Average Negativity Rate', icon="😔")
-            st.metric("Negativity Rate",negativity_rate)
-
-
-        import streamlit as st
-        import matplotlib.pyplot as plt
->>>>>>> 2c8fb1c438db844ce5d5cd2fba7fc2061add227a
 
         # Sample data
         labels = ["Positive", "Negative", "Neutral"]
